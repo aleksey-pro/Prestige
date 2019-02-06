bb6156fce755090275b119e122c0ab57753c01c5
davegahn <anxieter@yandex.ru>
2019-02-06 19:17:02 +0300

    New design

--- dev/js/menu.js
+++ dev/js/menu.js
@@ -1,33 +1,35 @@
+// import { isString } from "util";
+
 (function() {
-			var container = document.getElementById( 'container' ),
-				trigger = container.querySelector( 'button.trigger' );
+    var container = document.getElementById( 'container' ),
+        trigger = document.querySelector( '.trigger' );
 
-			function toggleContent() {
-				if( classie.has( container, 'container--open' ) ) {
-					classie.remove( container, 'container--open' );
-					classie.remove( trigger, 'trigger--active' );
-					window.addEventListener( 'scroll', noscroll );
-				}
-				else {
-					classie.add( container, 'container--open' );
-					classie.add( trigger, 'trigger--active' );
-					window.removeEventListener( 'scroll', noscroll );
-				}
-			}
 
-			function noscroll() {
-				window.scrollTo( 0, 0 );
-			}
+    function toggleContent() {
+        console.log('close');
 
-			// reset scrolling position
-			document.body.scrollTop = document.documentElement.scrollTop = 0;
+        if(container.classList.contains('container--open')){
+            container.classList.remove('container--open');
+            // window.addEventListener( 'scroll', noscroll );
+        } else {
+            console.log('open');
+            container.classList.add('container--open');
+            // window.removeEventListener( 'scroll', noscroll );
+        }
+    }
 
-			// disable scrolling
-			//window.addEventListener( 'scroll', noscroll );
-			
-			
-			trigger.addEventListener( 'click', toggleContent );
+    function noscroll() {
+        window.scrollTo( 0, 0 );
+    }
 
-			// For Demo purposes only (prevent jump on click)
-			//[].slice.call( document.querySelectorAll('.items-wrap a') ).forEach( function(el) { el.onclick = function() { return false; } } );
-		})();
+    // reset scrolling position
+    document.body.scrollTop = document.documentElement.scrollTop = 0;
+
+    // disable scrolling
+    // window.addEventListener( 'scroll', noscroll );
+
+    trigger.addEventListener( 'click', toggleContent );
+
+    // For Demo purposes only (prevent jump on click)
+    //[].slice.call( document.querySelectorAll('.items-wrap a') ).forEach( function(el) { el.onclick = function() { return false; } } );
+})();
