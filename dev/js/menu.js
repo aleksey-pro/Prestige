fbc9fb99fd2a278bb2a97f88d0df7a4a34d0f67a
davegahn <anxieter@yandex.ru>
2019-02-08 19:14:15 +0300

    Changes 2

--- dev/js/menu.js
+++ dev/js/menu.js
@@ -1,35 +1,5 @@
-// import { isString } from "util";
 
-(function() {
-    var container = document.getElementById( 'container' ),
-        trigger = document.querySelector( '.trigger' );
-
-
-    function toggleContent() {
-        console.log('close');
-
-        if(container.classList.contains('container--open')){
-            container.classList.remove('container--open');
-            // window.addEventListener( 'scroll', noscroll );
-        } else {
-            console.log('open');
-            container.classList.add('container--open');
-            // window.removeEventListener( 'scroll', noscroll );
-        }
-    }
-
-    function noscroll() {
-        window.scrollTo( 0, 0 );
-    }
-
-    // reset scrolling position
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
-
-    // disable scrolling
-    // window.addEventListener( 'scroll', noscroll );
-
-    trigger.addEventListener( 'click', toggleContent );
-
-    // For Demo purposes only (prevent jump on click)
-    //[].slice.call( document.querySelectorAll('.items-wrap a') ).forEach( function(el) { el.onclick = function() { return false; } } );
-})();
+$('.menu_button').click(function(){
+    var blockToToggle = $(this).closest('#container');
+    blockToToggle.toggleClass('container--open');
+});
--- prod/js/menu.js
+++ prod/js/menu.js
@@ -1,2 +1,2 @@
-!function(){function e(){console.log("close"),o.classList.contains("container--open")?o.classList.remove("container--open"):(console.log("open"),o.classList.add("container--open"))}var o=document.getElementById("container"),n=document.querySelector(".trigger");document.body.scrollTop=document.documentElement.scrollTop=0,n.addEventListener("click",e)}();
+$(".menu_button").click(function(){var n=$(this).closest("#container");n.toggleClass("container--open")});
 //# sourceMappingURL=menu.js.map
